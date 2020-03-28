d894a2614a17a7bffb92e78655ca3aa513135d68
Sergey Hovakimyan <hovakimyan.serg@gmail.com>
2020-03-28 16:21:06 +0400

    fix(src): add example boilerplate

---
+++ src/containers/root/index.js
@@ -0,0 +1,11 @@
+// @flow
+
+import React from 'react'
+
+const Root = (): React$Node => {
+    return (
+   <div>Hello from root</div>
+    )
+}
+
+export default Root
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.render(
+ <BrowserRouter basename="/">
+<div>asdfasdfa</div>
+  </BrowserRouter>,
+document.getElementById('root')
+)
+
